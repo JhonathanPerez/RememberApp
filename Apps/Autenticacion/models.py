79d345736e543f7d3276fa37828466aebb27cfea
Jhonathan <jhperez.ortega@gmail.com>
2019-09-12 10:07:19 -0500

    Se agrega el choice de genero en el modelos de autenticacion de la base de datos

--- Apps/Autenticacion/models.py
+++ Apps/Autenticacion/models.py
@@ -11,19 +11,26 @@ DOC_CHOICES = (
     ('DUI',(u"Documento Único de Identidad (DUI)")),
 )
 
+GEN_CHOICES = (
+    ('M',(u"Maculino (M)")),
+    ('F',(u"Femenino (F)")),
+    ('O',(u"Otro (O)")),
+)
+
+
 class DatosPersonales(models.Model):
     id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50, blank=True, null=True)
-    tipoid = models.CharField(max_length=20, blank=True, null=True, choices=DOC_CHOICES, default='CC')
-    numeroid = models.CharField(max_length=20, blank=True, null=True)
-    telefono = models.CharField(max_length=20, blank=True, null=True)
-    correo = models.CharField(max_length=50, blank=True, null=True)
-    foto = models.ImageField(upload_to='usuarios', default='usuarios/usuario.png')
+    nombre = models.CharField('Nombre', max_length=50)
+    apellido = models.CharField('Apellido', max_length=50)
+    tipoid = models.CharField('Tipo Identificacion', max_length=20,choices=DOC_CHOICES, default='CC')
+    numeroid = models.CharField('Numero de identificacion', max_length=20)
+    telefono = models.CharField('Telefono', max_length=20)
+    correo = models.CharField('Correo', max_length=50)
+    foto = models.ImageField('foto', upload_to='usuarios', default='usuarios/usuario.png')
     usuid = models.OneToOneField(User, on_delete = models.CASCADE)
 
     def __str__(self):
-        return "%s %s" % (self.datnombre, self.datapellido)
+        return "%s %s" % (self.nombre, self.apellido)
 
     class Meta:
         verbose_name_plural = "Datos Personales"
@@ -31,7 +38,7 @@ class DatosPersonales(models.Model):
 
 class Rol(models.Model):
     rolid = models.AutoField(primary_key=True)
-    roltipo = models.CharField(max_length=50)
+    roltipo = models.CharField('Tipo de rol', max_length=50)
 
     def __str__(self):
         return self.roltipo
