cf590468484ff4e5de29c34c8f628b359db0aa16
Jhonathan <jhperez.ortega@gmail.com>
2019-09-20 21:43:55 -0500

    Se corrigen errores en el cierre de sesión

--- Apps/Autenticacion/urls.py
+++ Apps/Autenticacion/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import Login
+from .views import Login, Logout
 
 urlpatterns = [
     path('',Login.as_view(), name = 'login'),
-    path('logout/',Login.as_view(), name = 'logout'),
+    path('logout/',Logout.as_view(), name = 'logout'),
 ]
