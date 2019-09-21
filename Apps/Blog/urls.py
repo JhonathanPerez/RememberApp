ad887aca465453dcefb3d320e8ae830ce579f2b7
Jhonathan <jhperez.ortega@gmail.com>
2019-09-20 20:11:10 -0500

    Se integra aplicaión cuidadores

--- Apps/Blog/urls.py
+++ Apps/Blog/urls.py
@@ -3,7 +3,7 @@ from .views import Inicio,DetallePost,Listado
 
 urlpatterns = [
     path('blog/',Inicio.as_view(), name = 'index'),
-    path('tratamientos/',Listado.as_view(),{'nombre_categoria':'tratamientos'}, name = 'tratamientos'),
-    path('generales/',Listado.as_view(),{'nombre_categoria':'General'}, name = 'generales'),
-    path('<slug:slug>/',DetallePost.as_view(), name = 'detalle_post'),
+    path('blog/tratamientos/',Listado.as_view(),{'nombre_categoria':'tratamientos'}, name = 'tratamientos'),
+    path('blog/generales/',Listado.as_view(),{'nombre_categoria':'General'}, name = 'generales'),
+    path('blog/<slug:slug>/',DetallePost.as_view(), name = 'detalle_post'),
 ]
